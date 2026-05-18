2f10c47a89fe2f03a6f8dfdc86a524314839a5c3
nhan <nhan01258688613@gmail.com>
2026-05-18 22:40:54 +0700

    fix render https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -28,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
             $view->with('cartCount', $cartCount);
 
         });
+
+            if (app()->environment('production')) {
+                URL::forceScheme('https');
+            }
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'customer' => App\Http\Middleware\CustomerMiddleware::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
