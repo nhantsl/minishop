668525bd9bca5323155e2d5255d9069478695a71
nhan <nhan01258688613@gmail.com>
2026-05-18 22:05:59 +0700

    fix factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,9 +25,6 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
